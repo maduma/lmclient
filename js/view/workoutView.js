06af6689b04c224233f4c2b7a1982fd316919d73
maduma <maduma@pt.lu>
2013-10-20 10:26:27 -0400

    add missing file

---
+++ js/view/workoutView.js
@@ -0,0 +1,19 @@
+define(["jquery", "backbone"], function( $, Backbone) {
+    var WorkoutView = Backbone.View.extend({
+        initialize: function() {
+            if(this.collection) {
+                this.collection.on( "added", this.render, this );
+            }
+        },
+        setCollection: function(collection) {
+            this.collection = collection;
+            this.collection.on( "added", this.render, this );
+        },
+        render: function() {
+            this.template = _.template( $( "script#workoutItems" ).html(), {"collection": this.collection});
+            this.$el.html(this.template);
+            return this;
+        }
+    });
+    return WorkoutView;
+});
